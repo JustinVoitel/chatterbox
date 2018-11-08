99f36155b4ae86474dae766fd5928b2cc327b0d5
Justin Voitel <justin.voitel@gmail.com>
2018-11-08 16:54:41 +0100

    update

--- Chatterbox/src/client/Client.java
+++ Chatterbox/src/client/Client.java
@@ -1,10 +1,52 @@
 package client;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Client extends Thread{
+	private static String host = "192.168.99.1";
+	private static int port = 8010; 
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		boolean active = true;
+		Scanner scanner = new Scanner(System.in);
+		
+	    Socket socket = new Socket(host,port);
+	    System.out.println("connected to server: "+host);
 
+	    OutputStream socketoutstr = socket.getOutputStream(); 
+	    OutputStreamWriter osr = new OutputStreamWriter( socketoutstr ); 
+	    BufferedWriter bw = new BufferedWriter( osr ); 
+
+	    InputStream socketinstr = socket.getInputStream(); 
+	    InputStreamReader isr = new InputStreamReader( socketinstr ); 	     
+	    BufferedReader br = new BufferedReader( isr ); 
+	    
+	    String anfrage;
+	    String antwort;
+	    while(active) {
+	    	
+	    	anfrage = scanner.nextLine();
+	    	bw.write(anfrage); 
+	    	bw.newLine(); 
+	    	bw.flush();
+	    	
+	    	antwort = br.readLine(); 
+	    	System.out.println(antwort); 
+	    	//System.out.println("Host = "+host); 
+	    	
+	    	
+	    }
+	    bw.close(); 
+	    br.close(); 
+	    socket.close();
 	}
 
 }
--- Chatterbox/src/server/Server.java
+++ Chatterbox/src/server/Server.java
@@ -1,10 +1,52 @@
 package server;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Server extends Thread{
+	private static int port = 8010;
 
+	public static void main(String[] args) throws IOException {
+		boolean active = true;
+		Scanner scanner = new Scanner(System.in);
+		ServerSocket serverSocket = new ServerSocket(port);
+		System.out.println("Server running @: "+InetAddress.getLocalHost().getHostAddress());
+		Socket clientSocket = serverSocket.accept();
+		OutputStream socketoutstr = clientSocket.getOutputStream(); 
+		OutputStreamWriter osr = new OutputStreamWriter( socketoutstr ); 
+		BufferedWriter bw = new BufferedWriter( osr ); 
+		
+		InputStream socketinstr = clientSocket.getInputStream(); 
+		InputStreamReader isr = new InputStreamReader( socketinstr ); 
+		BufferedReader br = new BufferedReader( isr ); 
+		
+		String anfrage; 
+		String antwort; 
+		while(active) {
+			
+			anfrage = br.readLine();
+			System.out.println(anfrage);
+			
+			antwort = scanner.nextLine(); 
+			bw.write(antwort); 
+			bw.newLine(); 
+			bw.flush(); 
+		}
+		
+
+		bw.close(); 
+		br.close(); 
+		clientSocket.close(); 
+		serverSocket.close();
 	}
 
 }
